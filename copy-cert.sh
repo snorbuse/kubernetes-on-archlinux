f207e111570b72f32238523e73ce600d41777760
Dread Eklund <dread.eklund@gmail.com>
2016-12-02 21:50:51 +0100

    Add Flannel as Kubernetes networking

--- copy-cert.sh
+++ copy-cert.sh
@@ -11,3 +11,12 @@ cp cert/rootCA.pem ansible/roles/master/files/
 mkdir -p ansible/roles/worker/files/ 2> /dev/null
 cp cert/kubernetesworker*.pem ansible/roles/worker/files/
 cp cert/rootCA.pem ansible/roles/worker/files/
+
+mkdir -p ansible/roles/flannel/files/ 2> /dev/null
+cp cert/kubernetesworker*.pem ansible/roles/flannel/files/
+cp cert/rootCA.pem ansible/roles/flannel/files/
+
+
+mkdir -p ansible/roles/flannel-register/files/ 2> /dev/null
+cp cert/kubernetesworker*.pem ansible/roles/flannel-register/files/
+cp cert/rootCA.pem ansible/roles/flannel-register/files/
